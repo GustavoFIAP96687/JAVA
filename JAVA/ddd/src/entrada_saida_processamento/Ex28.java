fbd3127146589cee48fd8332f11ec92b3f9edf9e
GustavoFIAP96687 <111809658+GustavoFIAP96687@users.noreply.github.com>
2022-09-06 22:58:23 -0300

    Add files via upload

--- JAVA/ddd/src/entrada_saida_processamento/Ex28.java
+++ JAVA/ddd/src/entrada_saida_processamento/Ex28.java
@@ -7,32 +7,32 @@ public class Ex28 {
 	public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 		
-		int n1, n2, n3;
+		int v1, v2, v3;
 		
 		System.out.printf("Digite o primeiro valor: ");
-		n1 = ler.nextInt();
+		v1 = ler.nextInt();
 		
 		System.out.printf("Digite o segundo valor: ");
-		n2 = ler.nextInt();
+		v2 = ler.nextInt();
 		
 		System.out.printf("Digite o terceiro valor: ");
-		n3 = ler.nextInt();
+		v3 = ler.nextInt();
 
-		if ((n1 < n2) && (n1 < n3)) {
-			if (n2 < n3)
-				System.out.printf("1- %d \n2- %d \n3- %d", n1, n2, n3);
+		if ((v1 < v2) && (v1 < v3)) {
+			if (v2 < v3)
+				System.out.printf("1- %d \n2- %d \n3- %d", v1, v2, v3);
 			else
-				System.out.printf("1- %d \n2- %d \n3- %d", n1, n3, n2);
-	   }else if ((n2 < n1) && (n2 < n3)) {
-		   if (n1 < n3) 
-			    System.out.printf("1- %d \n2- %d \n3- %d", n2, n1, n3); 
+				System.out.printf("1- %d \n2- %d \n3- %d", v1, v3, v2);
+	   }else if ((v2 < v1) && (v2 < v3)) {
+		   if (v1 < v3) 
+			    System.out.printf("1- %d \n2- %d \n3- %d", v2, v1, v3); 
 		   else 
-			    System.out.printf("1- %d \n2- %d \n3- %d", n2, n3, n1);
+			    System.out.printf("1- %d \n2- %d \n3- %d", v2, v3, v1);
            }else {
-        	   if (n1 < n2) {
-                System.out.printf("1- %d \n2- %d \n3- %d", n3, n1, n2);   
+        	   if (v1 < v2) {
+                System.out.printf("1- %d \n2- %d \n3- %d", v3, v1, v2);   
         	   }else {
-        	    System.out.printf("1- %d \n2- %d \n3- %d", n3, n2, n1);
+        	    System.out.printf("1- %d \n2- %d \n3- %d", v3, v2, v1);
         	   }
            }
 		   
